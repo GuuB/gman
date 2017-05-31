4a261bf685a2c5106945a87ec41fb2f403da7bd8
GuuB <yugbooth@hotmail.com>
2017-05-31 18:34:45 -0400

    first commit

---
+++ gman/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gman.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("about")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("skills")]
+        public IActionResult Skills()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("projects")]
+        public IActionResult Projects()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("contact")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ gman/wwwroot/js/gman.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+
+    $("#name").slideDown().show(function () {
+        $("#info").slideDown().show();
+    });
+
+    var classes = new Array('colorchange pink', 'colorchange grey', 'colorchange black', 'colorchange blue', 'colorchange green', 'colorchange white', 'colorchange red');
+    var length = classes.length;
+
+    $(".colorchange").on("mouseover", function () {
+        $(this).removeClass().addClass(classes[Math.floor(Math.random() * length)]);
+
+        $(this).delay(1500).queue(function (next) {
+            $(this).removeClass().addClass("colorchange");
+            next();
+        })
+    })
+
+
+    $("#info2").delay(10000).queue(function (next) {
+        $("#info").slideUp(function () {
+            $("#info2").slideDown().show();
+        })
+        next();
+    })
+
+    //$("#skills").on("click", function (e) {
+    //    e.preventDefault();
+    //    $("#info").slideUp(function () {
+    //        $("#info2").slideDown().show();
+    //    });
+    //})
+})
+
+
+
+
